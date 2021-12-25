e627769851dd1e7488ab825a922131d23a363dab
hh-beep <75884889+hh-beep@users.noreply.github.com>
2021-12-25 11:13:57 -0300

    gg ez

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from "../../images/logo.png"
+import "./styles/Footer.css"
+
+function Footer ({ redirect }) {
+  return(
+    <footer className='footer' >
+      <figure className='footer__container' >
+        <img className='footer__container-image' src={Logo} onClick={ () => {
+          redirect()
+        }} />
+      </figure>
+      <h1 className='footer__copy' >&copy; Hp shop</h1>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from "../../images/logo.png"
+import "./styles/Header.css"
+
+function Header({ redirect }) {
+
+  const routes = [
+    {name: "Home", path: "/"},
+    {name: "Cart", path: "/Cart"},
+    {name: "Profile", path: "/Profile"},
+    {name: "About Us", path: "/AboutUs"}
+  ]
+
+  return(
+    <header className="header" >
+
+    <figure className="header__container">
+      <img className="header__container-image" src={Logo} onClick={ () => {
+        redirect()
+      }} />
+    </figure>
+
+      <ul className="header__list" >
+        {routes.map( item => {
+          return(
+            <li className='header__list-item item'>
+              <Link className='item__link' to={ item.path } >{ item.name }</Link>
+            </li>
+          )
+        })}
+      </ul>
+
+    </header>
+  )
+}
+
+export default Header;
