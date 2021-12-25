96cd62022a7209116f899049c7434470a337dd16
hh-beep <75884889+hh-beep@users.noreply.github.com>
2021-12-25 14:45:33 -0300

    changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Logo from "../../images/logo.png"
 import "./styles/Footer.css"
 
-function Footer ({ redirect }) {
+function Footer () {
   return(
     <footer className='footer' >
       <figure className='footer__container' >
-        <img className='footer__container-image' src={Logo} />
+        <img className='footer__container-image' src={Logo} onClick={
+          console.log(window)
+        } />
       </figure>
       <h1 className='footer__copy' >&copy; Hp shop</h1>
     </footer>
